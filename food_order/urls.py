c764ddadc9f00c1f9e8f146d837bbb5e261d7560
Manikandan A <115366703+Manikandan16A@users.noreply.github.com>
2024-12-11 16:04:37 +0530

    Update urls.py

--- food_order/urls.py
+++ food_order/urls.py
@@ -1,11 +1,15 @@
 from . import views  # Ensure this import is correct
 from django.http import HttpResponse
 from django.urls import path, include
+from django.contrib import admin
+
 
 def home(request):
     return HttpResponse("Welcome to Food Order API!")
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path('', home, name='home'),  # Root URL
     path('api/food-items/', include('api.urls')),  # API paths
 ]
