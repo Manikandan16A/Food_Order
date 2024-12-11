d1676cf7a2c9ecd25d9616090e5f296b1f23c5a0
Manikandan A <115366703+Manikandan16A@users.noreply.github.com>
2024-12-11 20:14:49 +0530

    Delete api directory

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from ..food_order.models import FoodItem
-
-class FoodItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FoodItem
-        fields = ['id', 'name', 'price', 'image_url']
--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('food-items/', views.food_items_list, name='food_items'),  # Add the 'food-items/' path
-]
