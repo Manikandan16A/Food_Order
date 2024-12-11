12ef34924e2089e5ff8e43c59001192667edced3
Manikandan A <115366703+Manikandan16A@users.noreply.github.com>
2024-12-11 22:55:45 +0530

    Add files via upload

---
+++ orders/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import FoodItem
+from .serializers import FoodItemSerializer
+
+class FoodItemViewSet(viewsets.ModelViewSet):
+    queryset = FoodItem.objects.all()
+    serializer_class = FoodItemSerializer
