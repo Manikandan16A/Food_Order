b7dac6f0921ea4c60c3f60503190d37cdb045a9a
Manikandan A <115366703+Manikandan16A@users.noreply.github.com>
2024-12-12 00:41:33 +0530

    Delete orders directory

--- orders/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import FoodItem
-from .serializers import FoodItemSerializer
-
-class FoodItemViewSet(viewsets.ModelViewSet):
-    queryset = FoodItem.objects.all()
-    serializer_class = FoodItemSerializer
