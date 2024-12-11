918ab686aac54e9a8acf41b17076a83563f99802
Manikandan A <115366703+Manikandan16A@users.noreply.github.com>
2024-12-11 22:53:54 +0530

    Delete food_order directory

--- food_order/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import FoodItem
-
-class FoodItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FoodItem
-        fields = ['name', 'price', 'image_url']
